5584df9aa81c2dd3f009ebbaab57059654119fcc
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-02-08 17:35:28 +0200

    Adding routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import http from 'http'
+import routes from './src/routes/mainRoutes.js'
 
 // express app
 const app = express()
@@ -15,7 +16,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-
+app.use('/api/movie', routes)
 
 const server = http.createServer(app)
 
