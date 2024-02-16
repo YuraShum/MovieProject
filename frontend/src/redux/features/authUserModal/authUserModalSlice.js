c2f7c29a85e223830bc8d1f36ea2999d9dabf689
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-02-16 14:01:48 +0200

    fix authUserModal global state

--- frontend/src/redux/features/authUserModal/authUserModalSlice.js
+++ frontend/src/redux/features/authUserModal/authUserModalSlice.js
@@ -1,18 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    authModalOpen: false
+    authUserModal: false
 }
 
 export const authUserModalSlice = createSlice({
     name: 'authUserModal',
     initialState,
     reducers: {
-        setAuthModalOpen: (state, action) => {
-            state.authModalOpen = action.payload
+        setAuthUserModal: (state, action) => {
+            state.authUserModal = action.payload
         }
     }
 })
 
-export const { setAuthModalOpen } = authUserModalSlice.actions
+export const { setAuthUserModal } = authUserModalSlice.actions
 export default authUserModalSlice.reducer
