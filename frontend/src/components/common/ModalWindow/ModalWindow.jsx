2660b020b8e9babc23b62b44666e1ca964f5a64f
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-03-11 22:27:52 +0200

    added gradient to  modal window

--- frontend/src/components/common/ModalWindow/ModalWindow.jsx
+++ frontend/src/components/common/ModalWindow/ModalWindow.jsx
@@ -2,14 +2,16 @@ import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { MODAL_AUTH } from "../../../const/constConfig"
 import { setAuthUserModal } from "../../../redux/features/authUserModal/authUserModalSlice"
-import { Box, Button, Modal, Typography } from "@mui/material"
+import { Box, Button, Modal, Typography, useTheme } from "@mui/material"
 import { FaCircle } from "react-icons/fa";
 import Signin from "../Form/Signin/Signin"
 import Signup from "../Form/Signup/Signup"
+import { themeState } from "../../../styles/theme"
 
 
 
 const ModalWindow = () => {
+    const theme = useTheme()
     const dispatch = useDispatch()
     const { authUserModal } = useSelector((state) => state.authUserModal)
 
@@ -24,14 +26,10 @@ const ModalWindow = () => {
     const handleCloseModal = () => {
         dispatch(setAuthUserModal(false))
     }
-    const switchActionState = (state) => {
-        setAction(state)
-    }
-
 
     return (
         <Modal
-        
+
             open={authUserModal !== undefined ? authUserModal : false}
             onClose={handleCloseModal}
         >
@@ -47,12 +45,12 @@ const ModalWindow = () => {
                     backgroundColor: 'background.paper',
                     boxShadow: 12,
                     borderRadius: '10px',
-                    
+
 
                 }}>
 
                 <Box
-                    sx={{ display: "flex", justifyContent: 'space-between', marginBottom: '2.5rem', gap: '1rem'}}>
+                    sx={{ display: "flex", justifyContent: 'space-between', marginBottom: '2.5rem', gap: '1rem' }}>
                     <Button
                         variant="contained"
                         onClick={() => setAction(MODAL_AUTH.signin)}>
@@ -67,14 +65,18 @@ const ModalWindow = () => {
                             maxWidth: {
                                 xs: '30%',
                                 md: '60%'
-                                
+
                             },
                             width: '100%',
                             borderRadius: '20px',
                             display: 'flex',
                             alignItems: 'center',
                             position: 'relative',
-
+                            transition: 'all 0.6s ease',
+                            backgroundImage: action === MODAL_AUTH.signup ?
+                                `linear-gradient(to right, ${theme.palette.mode === themeState.light ? 'white': '#131313'} 60%, #2196f3 100%)`
+                                :
+                                `linear-gradient(to left, ${theme.palette.mode === themeState.light ? 'white': '#131313'}  60%, #2196f3 100%)`
                         }}>
                         <FaCircle
                             style={{
